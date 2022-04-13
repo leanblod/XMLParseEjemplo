121bf1df012fb91a4507d8ba31bd55333a49eed7
I2T-LAVALOS\LISANDRO AVALOS <lisandro.avalos@i2tsa.com.ar>
2022-04-13 12:22:10 -0300

    Commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'PruebaXML';
-}
+import { Component } from '@angular/core';  
+import * as xml2js from 'xml2js';  
+import { HttpClient, HttpHeaders } from '@angular/common/http';  
+@Component({  
+  selector: 'app-root',  
+  templateUrl: './app.component.html',  
+  styleUrls: ['./app.component.css']  
+})  
+export class AppComponent {  
+  title = 'read-xml-angular8';  
+  public xmlItems: any;  
+  constructor(private _http: HttpClient) { this.loadXML(); }
+  loadXML() {
+    this._http.get('http://restapi.adequateshop.com/api/Traveler',  
+      {  
+        headers: new HttpHeaders()  
+          .set('Content-Type', 'text/xml')  
+          .append('Access-Control-Allow-Methods', 'GET')  
+          .append('Access-Control-Allow-Origin', '*')  
+          .append('Access-Control-Allow-Headers', "Access-Control-Allow-Headers, Access-Control-Allow-Origin, Access-Control-Request-Method")
+          .append('Accept','application/xml'),  
+        responseType: 'text',
+      }
+      ).subscribe((data) => {  
+        this.parseXML(data)  
+          .then((data) => {  
+            this.xmlItems = data;  
+          });  
+      });  
+  }  
+  parseXML(data: any) {  
+    return new Promise(resolve => {  
+      var k: string | number,  
+        arr: any = [],  
+        parser = new xml2js.Parser(  
+          {  
+            trim: true,  
+            explicitArray: true  
+          });  
+      parser.parseString(data, function (err: any, result: any) {
+        var lista = result.TravelerinformationResponse.travelers[0].Travelerinformation;
+        console.log(parser);
+        for (let item of lista) {
+          arr.push({  
+            id: item.id[0],
+            name: item.name[0],
+            email: item.email[0],
+            adderes: item.adderes[0],
+            createdat: item.createdat[0],
+          });
+        }  
+        resolve(arr);  
+      });  
+    });  
+  }  
+} 
